130311362c89433f98911a0aaa491bd5d6535a26
Uriel Avalos <uri@frankandrobot.com>
2016-01-17 20:20:18 -0600

    [variable] generator sort of works

--- test/input5.ts
+++ test/input5.ts
@@ -1,1 +1,14 @@
-export const foo = 1234, bar = 'hi'
+export const one = 1234, two = 'twoValue'
+export let three = 'threeValue'
+export var four = 5678
+export const five = <string>'fiveValue'
+export const six = 60 as any as string
+export const seven = 'sevenValue' as string
+export const eight : string = seven
+export const nine : IBar = {} as IBar
+export const ten = {}
+export const eleven = 1 + 2
+export const twelve = 120 as any as string
+export const thirteen = two + three
+
+interface IBar {}
